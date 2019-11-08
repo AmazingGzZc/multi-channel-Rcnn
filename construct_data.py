164dbabaf7e4a01a28cab548d7824e66e76a83d2
AmazingGzZc <447511600@qq.com>
2019-11-08 18:02:42 +0800

    构建自身数据集并加载

--- construct_data.py
+++ construct_data.py
@@ -1,102 +1,59 @@
-from predata import DIC
 import torch
-from torch.utils.data.dataset import Dataset
 import numpy as np
+from gensim.models import word2vec
+from torch.utils.data.dataset import Dataset
 import torch.utils.data as d
-class traingzc(Dataset):
-    def __init__(self,word2index,index2word,file_path,label_path,NW,NS,PW,PS):
-        self.word2index=word2index
-        self.index2word=index2word
+model=word2vec.Word2Vec.load('word2vec训练好的词向量.model')#此处的加载的model文件需对应自己的任务进行训练
+class trainData(Dataset):
+    def __init__(self,file_path,label_path):
         self.file_path=file_path
         self.label_path=label_path
-        self.NW=NW   #负面词语
-        self.NS=NS   #负面情感
-        self.PW=PW   #正面词语
-        self.PS=PS   #正面情感
-        self.Max_length=300
         file1=open(self.file_path,'r')
         file2=open(self.label_path,'r')
-        self.comment=[]
+        self.comment=[]   #此处就是将训练数据示例.txt中的4条文本放在列表中，如果您有10000条，那么将10000条文本放入到列表中
         for line in file1.readlines():
             self.comment.append(line)
-        self.labels=[]
+        self.labels=[]    #此处是将对应的标签放入到列表中
         for num in file2.readlines():
-            self.labels.append(num.split(' ')[0])
+            self.labels.append(num)
     def __getitem__(self,index):
-        data=self.comment[index]
-        label=self.labels[index]
-        sen=data.split(' ')
-        sens=[]
-        word_features=[]
-        for word in sen:
-            if word!='\n':
-                sens.append(self.word2index[word])
-                if word in self.NW:
-                    word_features.append(1)
-                elif word in self.NS:
-                    word_features.append(2)
-                elif word in self.PW:
-                    word_features.append(3)
-                elif word in self.PS:
-                    word_features.append(4)
-                else:
-                    word_features.append(5)
-        if len(sens)<self.Max_length:
-            for i in range(self.Max_length-len(sens)):
-                sens.append(0)
-                word_features.append(0)
+        data=self.comment[index]  #此处的index即会循环遍历self.comment中的所有文本
+        label=self.labels[index]  #此处是对应的标签
+        data=data.split(' ')      #以下内容根据自身任务自行书写
+        com=np.zeros((300,200))
+        if len(data)<=300:
+            i=0
+            for word in data:
+                if word !='\n':
+                    com[i]=model[word]
+                    i+=1
         else:
-            sens=sens[:self.Max_length]
-            word_features=word_features[:self.Max_length]
-        if label == '1':
-            label=0
-        elif label == '0':
-            label=1
-        elif label == '-1':
-            label=2
-        else:
-            label=3
-        return torch.LongTensor([sens,word_features]),torch.Tensor([label]).squeeze()
-    def __len__(self):
+            i=0
+            for word in data:
+                if word != '\n':
+                    com[i]=model[word]
+                    i+=1
+                    if i ==300:
+                        break
+        #print(com)
+        lab=[]
+        label=label.split()
+        j=0
+        for num in label:
+            if num == '1':
+                lab.append(0)
+            elif num == '0':
+                lab.append(1)
+            elif num == '-1':
+                lab.append(2)
+            else:
+                lab.append(3)
+            j+=1
+        return torch.FloatTensor(com),torch.Tensor(lab)    #此处返回的是构建好一条数据及其对应的标签。
+    def __len__(self):    #此方法返回共有多少个数据
         return len(self.labels)
-'''word_name=['负面评价词语（中文）.txt','负面情感词语（中文）.txt','正面评价词语（中文）.txt','正面情感词语（中文）.txt']
-word_list=[[],[],[],[]]
-i=0
-for x in word_list:
-    f=open('/home/gzc/pytorh练习/multi-channel-Rcnn/hownet/'+word_name[i],'r')
-    for line in f.readlines():
-        x.append(line.strip('\n'+' '))
-    i+=1
-A=word_list[0]
-B=word_list[1]
-C=word_list[2]
-D=word_list[3]
-dic=DIC('测试评论+验证评论.txt')
-w2i,i2w=dic.addsentence()
-print(len(i2w))'''
-'''train_data=traingzc(w2i,i2w,'细粒度评论.txt','细粒度标签.txt',A,B,C,D)
-train_loader=d.DataLoader(dataset=train_data,batch_size=5,shuffle=True)
+train_data=trainData('训练数据示例.txt','训练数据标签.txt')
+train_loader=d.DataLoader(dataset=train_data,batch_size=2,shuffle=True)#通过dataloader方法可以批量导入数据，将数据输入到神经网络。
 for step,(x,y) in enumerate(train_loader):
     print(x,x.shape)
-    print(y,len(y))'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print(y,y.shape)
